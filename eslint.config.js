fd1846d03e5f8b4ad964cd6f4ae142f18391d5f2
Brian Shef <brianshef@outlook.com>
2026-07-15 23:39:56 -0500

    Configure linter

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default {
+  root: true,
+  env: {
+    node: true,
+    es2024: true,
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-undef': 'error',
+    'no-console': 'off',
+  },
+};
