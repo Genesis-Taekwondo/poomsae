e53dac0a0b193a8ce2e58b990603d7981b16fcc3
Brian Shef <brianshef@outlook.com>
2026-07-16 22:57:45 -0500

    Fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,35 @@
-export default {
-  languageOptions: {
-    ecmaVersion: 2024,
-    sourceType: 'module',
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+      globals: {
+        require: 'readonly',
+        module: 'readonly',
+        exports: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        process: 'readonly',
+        console: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+      'no-console': 'off',
+    },
   },
-  environments: {
-    node: true,
+  {
+    files: ['test/**/*.js'],
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        before: 'readonly',
+        after: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+      },
+    },
   },
-  rules: {
-    'no-unused-vars': 'warn',
-    'no-undef': 'error',
-    'no-console': 'off',
-  },
-};
+];
